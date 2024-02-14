971b1297c0098f690c966ab59fb24b7b4d9297b6
Nathan Harris <nathan.harris.1@outlook.com>
2024-02-14 22:23:23 +0000

    makes placeholder pdfs with details in filenames

--- ingest_hardcopies.py
+++ ingest_hardcopies.py
@@ -5,20 +5,114 @@ Created on 2024-02-14
 """
 import os
 
+import time
+from datetime import datetime
 from libib_records.ingest_libib import get_book_records as get_books
+from pdf.make_pdf import create_pdf
 
 marker = 'marker'
 
 def get_path(collection):
     return os.getenv(collection)
 
+
+def strip_book_data(complete_data):
+    data_keys = complete_data.keys()
+    stripped_data = {}
+    key_data =["title" ,    "creators",    "first_name",    "last_name",    "ean_isbn13",    "upc_isbn10"]
+    for key in key_data:
+        if key not in data_keys:
+            pass
+        else:
+            stripped_data[key] = complete_data[key]
+    if "title" in stripped_data:
+        stripped_data["unknown"] = False
+        return stripped_data
+    else:
+        return {"unknown": True}
+
+
+def time_ext():
+    now = datetime.now()
+    milliseconds = int(now.microsecond / 1000)
+    formatted_time = now.strftime("%H%M%S") + '{:03}'.format(milliseconds)
+    time.sleep(0.001)
+    return formatted_time
+
+
+class BookPDF:
+    def __init__(self, data, folder_out, owner=""):
+        self.data = data
+        self.out_folder = folder_out
+        self.owner = owner
+        self.metadata_set = {}
+        self.filename = "title?@authors?@isbn0@"
+        self.output_path = "pdf/book_pdfs"
+        self.post_init()
+
+    def make_filename(self):
+        name_string = ""
+        title_string = "title" + self.data["title"] if self.data["title"] else "?"
+        if self.data["first_name"] and self.data["last_name"]:
+            author_string = self.data["first_name"] + " " + self.data["last_name"]
+        else:
+            author_string = "?"
+        author = "authors" + \
+            self.data["creators"] if self.data["creators"] else author_string
+        
+        isbn = self.data["upc_isbn10"] if self.data["upc_isbn10"] else "0"
+        isbn = "isbn" +  self.data["ean_isbn13"] if self.data["ean_isbn13"] else isbn
+
+        def cleanse_string(info_string):
+            if ":" in info_string:
+                info_string = info_string.replace(":", "-")
+            if "@" in info_string:
+                info_string = info_string.replace("@", " ")
+            return info_string
+        
+        title_string = cleanse_string(title_string)
+        author = cleanse_string(author)
+        isbn = cleanse_string(isbn)
+
+        name_string += title_string + "@"
+        name_string += author + "@"
+        name_string += isbn + "@"
+        return name_string
+
+    def post_init(self):
+        if not self.data["unknown"]:
+            self.filename = self.make_filename()
+            print(self.filename)
+        else:
+            self.filename = self.filename + time_ext()
+
+   
+    def make_pdf(self):
+        out_path = os.path.join(self.output_path, self.filename) + ".pdf"
+        print(out_path)
+        create_pdf(out_path, owner=self.owner)
+        # print(self.filename, "filename")
+    def add_metadata(self):
+        print("now here")
+
+
 def main():
     this_file_name = os.path.basename(__file__)
     print("File being run is:", this_file_name)
     #
+    owner = os.getenv("OWNER_TAG")  # ! MODIFY OWNER_TAG FOR NAME
+    #
     csv_path = get_path(collection="TEST_INCOMING_CSV")
     hardcopies = get_books(csv_path=csv_path)
-    print(hardcopies)
+    for hardcopy in hardcopies:
+        data = strip_book_data(hardcopy)
+        book = BookPDF(data, folder_out="pdf/book_pdfs", owner=owner)
+        book.make_pdf()
+        book.add_metadata()
+
+    #
+    # out_path = r"pdf\book_pdfs\test.pdf"
+    # create_pdf(out_path)
 
 if __name__ == '__main__':
     main()
--- pdf/make_pdf.py
+++ pdf/make_pdf.py
@@ -6,17 +6,27 @@ Created on 2024-02-14
 import os
 from reportlab.pdfgen import canvas
 
-def create_pdf(out_path):
+def create_pdf(out_path, owner=""):
+    msg = "This is a placeholder pdf representing real book"
+    owner_string = f"Owner: {owner}" if owner else f"Owner: ?"
+    text_lines = [msg, owner_string]
+
     c = canvas.Canvas(out_path)
-    c.setFont("Helvetica", 12)
-    c.drawString(100, 750, "Hello, World!")
+
+    text = c.beginText(100, 750)
+    c.setFont("Helvetica", 15)
+
+    for line in text_lines:
+        text.textLine(line)
+    c.drawText(text)
+
     c.save()
 
 def main():
     this_file_name = os.path.basename(__file__)
     print("File being run is:", this_file_name)
     out_path = r"pdf\book_pdfs\test.pdf"
-    create_pdf(out_path)
+    create_pdf(out_path, owner = "me")
 
 if __name__ == '__main__':
     main()
