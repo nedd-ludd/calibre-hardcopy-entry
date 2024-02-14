5479d0618cdd7261ea437669c7dfe891c2b76922
Nathan Harris <nathan.harris.1@outlook.com>
2024-02-14 22:36:18 +0000

    adds search of db for tags

--- db/find_paths.py
+++ db/find_paths.py
@@ -3,15 +3,49 @@
 """
 Created on 2024-02-14
 """
+import sqlite3
 import os
-marker = 'marker'
-def func():
-    pass
+
+# TODO: Want book folder not image
+# TODO: how do i combine tags searching in sqlite query
+# TODO: context manager for sqlite query
+# TODO: return list of folders
+
+def get_book_folders(db_path, user_tag, format):
+
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    query = '''
+    SELECT b.title, b.path || '/cover.jpg' AS cover_path
+    FROM books b
+    JOIN books_tags_link btl ON b.id = btl.book
+    JOIN tags t ON btl.tag = t.id
+    WHERE t.name = ?
+    '''
+    c.execute(query, (user_tag,))
+    results = c.fetchall()
+    if results:
+        for title, cover_path in results:
+            print(f"Title: {title}, Cover Path: {cover_path}")
+    else:
+        print(f"No books found with the tag '{user_tag}'.")
+
+    conn.close()
+    return [1,2]
 
 def main():
     this_file_name = os.path.basename(__file__)
     print("File being run is:", this_file_name)
-    func()
+    ut1 = os.getenv("USER_1_TAG")
+    ut2 = os.getenv("USER_2_TAG")
+    format_tag = "Hardcopy"
+    user_tag = ut1
+
+    db_path = os.getenv("TEST_CALIBRE_DB_PATH")
+
+    book_folders = get_book_folders(db_path, user_tag, format_tag)
+    print(book_folders)
 
 if __name__ == '__main__':
     main()
+
